f662079c337f49d3c7125dfb07e9cb55a2b18c89
varkasaapp <varkasaapps@gmail.com>
2018-03-08 11:01:41 +0530

    recent

--- app/src/main/java/com/example/admin/lunchbox/Categories.java
+++ app/src/main/java/com/example/admin/lunchbox/Categories.java
@@ -2,12 +2,18 @@ package com.example.admin.lunchbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class Categories extends AppCompatActivity {
-
+    GridView gridView;
+    int[] images={R.drawable.pic1,R.drawable.pic2,R.drawable.pic3,R.drawable.pic4};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.categories);
+        gridView=(GridView)findViewById(R.id.grid);
+        CustomAdapter customAdapter=new CustomAdapter(getApplicationContext(),images);
+        
+
     }
 }
